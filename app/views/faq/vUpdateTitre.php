6db013c6514c765b6f91c40cbe39ffdffa5e2b47
aclavel <antoine.clavel@sts-sio-caen.info>
2015-09-21 12:07:06 +0200

    admin/faq : 50%

--- app/views/faq/vUpdateTitre.php
+++ app/views/faq/vUpdateTitre.php
@@ -3,7 +3,8 @@
 		<label for="titre">Titre : </label>
 		<input type="hidden" id="id" name="id" value="<?=$faq->getId()?>">
 		<input type="text" class="form-control" id="titre" name="titre" value="<?=$faq->getTitre()?>">
+		<textarea name="description" id="description" placeholder="Entrez la question" class="form-control"><?php echo $faq->getContenu()?></textarea>
 	</div>
-	<a class="btn btn-primary" id="btUpdateTitre">Modifier</a>
-	<a href="" class="btn btn-primary" id="btUpdateTitre">Retour</a>
+	<a class="btn btn-primary" id="btUpdateTitre"><?php echo $ajou_modif?></a>
+	<a href="faqs" class="btn btn-primary" id="btUpdateTitre">Retour</a>
 </form>
