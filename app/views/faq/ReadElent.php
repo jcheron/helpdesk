9e8067b4064ea3f9502f21fa938f03cac0b970ec
apolline.mallet <apolline.mallet@sts-sio-caen.info>
2015-09-21 12:01:02 +0200

    ReadElent v1

--- app/views/faq/ReadElent.php
+++ app/views/faq/ReadElent.php
@@ -3,4 +3,6 @@
 		<label for="titreFAQ"><?= $titre->getTitre() ?></label>
 		<p id="contenuFAQ"><?= $contenu->getContenu()?></p>
 	</div>
+	
+	<a href="" class="btn btn-primary" id="btReadElent">Retour</a>
 </form>
